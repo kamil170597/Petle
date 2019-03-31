b478820c90c653317f0ce52a906fb96fd5541b83
kamil170597 <48652200+kamil170597@users.noreply.github.com>
2019-03-31 16:41:42 +0200

    Update Program.cs

--- Petle_zadania/Petle/Program.cs
+++ Petle_zadania/Petle/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Petle
 {
     class Program
     {
-        static void Main(string[] args)
+        const char CHAR = '*';
+        static void Star() => Console.Write(CHAR);
+        static void StarLn() => Console.WriteLine(CHAR);
+        static void Space() => Console.Write(" ");
+        static void SpaceLn() => Console.WriteLine(" ");
+        static void NewLine() => Console.WriteLine();
+
+        public static void Prostokat(int n, int m)
         {
+            for (int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
+
+            for (int j = 1; j < m - 1; j++)
+            {
+                Star();
+                for (int i = 1; i < n - 1; i++)
+                    Space();
+
+                StarLn();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Star();
+            }
+            NewLine();
         }
+
+        public static void Main(string[] args)
+        {
+            Prostokat(5, 7);
+        }
+
     }
 }
